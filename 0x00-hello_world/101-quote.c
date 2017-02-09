075e0e86a3e9bcad971ce27e2b8ca54bcea743a7
Stuart <108@holbertonschool.com>
2017-02-09 04:51:39 +0000

    fix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,9 +9,7 @@
 
 int main(void)
 {
-	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-
-	fwrite(str, sizeof(*str), strlen(str), stderr);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59)
 
 	return (1);
 }

8b9dffe65e7e9eb29babddeae6e8703e160236d3
Stuart <108@holbertonschool.com>
2017-02-11 00:20:35 +0000

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
-int main()
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
 {
 	int i;
 	int j;
@@ -17,9 +23,8 @@ int main()
 			putchar(' ');
 			putchar(j / 10 + '0');
 			putchar(j % 10 + '0');
-			if(i < 98)
+			if (i < 98)
 			{
-				//			getchar();
 				putchar(',');
 				putchar(' ');
 			}

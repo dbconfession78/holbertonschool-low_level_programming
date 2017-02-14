9b975370d295cf21ff3e9d1c897176a7e7f7b24f
Stuart <108@holbertonschool.com>
2017-02-14 20:51:57 +0000

    change vars to a and b instead of n1 and n2

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,14 +2,13 @@
 
 /**
  * add - adds two integers
- *
- *@n1: first of two ints to be added together
- *@n2: second of two ints to be added together
+ *@a: first of two ints to be added together
+ *@b: second of two ints to be added together
  *
  *Return: result of n1 + n2
  */
 
-int add(int n1, int n2)
+int add(int a, int b)
 {
-	return (n1 + n2);
+	return (a + b);
 }

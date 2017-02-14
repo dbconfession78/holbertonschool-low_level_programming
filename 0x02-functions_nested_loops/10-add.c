32e8a9df650935aaa4caf17426eda532ea2e6d1c
Stuart <108@holbertonschool.com>
2017-02-14 20:58:28 +0000

    make int unsigned

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,15 +2,15 @@
 
 /**
  * add - adds two integers
- *@a: first of two ints to be added together
- *@b: second of two ints to be added together
+ * @a: first of two ints to be added together
+ * @b: second of two ints to be added together
  *
- *Return: result of n1 + n2
+ * Return: result of n1 + n2
  */
 
 int add(int a, int b)
 {
-	int sum;
+	unsigned int sum;
 
 	sum = a + b;
 

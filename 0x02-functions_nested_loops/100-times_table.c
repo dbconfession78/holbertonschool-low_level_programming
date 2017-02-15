a14f196edadd359b2da37c04c29dd44c50d2c349
Stuart <108@holbertonschool.com>
2017-02-15 00:26:00 +0000

    change condition to enter function

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,31 +9,32 @@ void print_times_table(int n)
 {
 	int i, j, result, m1, m2, m3;
 
-	if (n > 15 || n < 0)
-		return;
-	for (i = 0; i <= n ; i++)
+	if (n < 16 && n > -1)
 	{
-		_putchar('0');
-		for (j = 1; j <= n; j++)
+		for (i = 0; i <= n ; i++)
 		{
-			result = i * j;
-			m1 = result / 100;	m2 = result / 10 % 10;	m3 = result % 10;
-			_putchar(',');
-			_putchar(' ');
-			if (result < 10)
+			_putchar('0');
+			for (j = 1; j <= n; j++)
 			{
-				_putchar(' ');	_putchar(' ');
-				_putchar(m3 +  '0');
+				result = i * j;
+				m1 = result / 100;	m2 = result / 10 % 10;	m3 = result % 10;
+				_putchar(',');
+				_putchar(' ');
+				if (result < 10)
+				{
+					_putchar(' ');	_putchar(' ');
+					_putchar(m3 +  '0');
+				}
+				else if (result >= 100)
+				{
+					_putchar(m1 + '0');	_putchar(m2 + '0');	_putchar(m3 + '0');
+				}
+				else
+				{
+					_putchar(' ');	_putchar(m2 + '0');	_putchar(m3 + '0');
+				}
 			}
-			else if (result >= 100)
-			{
-				_putchar(m1 + '0');	_putchar(m2 + '0');	_putchar(m3 + '0');
-			}
-			else
-			{
-				_putchar(' ');	_putchar(m2 + '0');	_putchar(m3 + '0');
-			}
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
 }

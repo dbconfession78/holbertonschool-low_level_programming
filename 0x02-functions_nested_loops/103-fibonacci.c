7e8e48687281786de7801acd4cf2fb3a9e75c9f0
Stuart <108@holbertonschool.com>
2017-02-15 04:54:14 +0000

    add 103-fibonacci.c

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: always return (0)
+ */
+
+int main(void)
+{
+	unsigned long i;
+	unsigned long a, b, c;
+	int sum;
+
+	a = 1;
+	b = 2;
+	sum = 0;
+
+	for (i = 3; c <= 4000000; i++)
+	{
+		c = a + b;
+		if (c > 4000000)
+		{
+			break;
+		}
+
+		if (c % 2 == 0)
+		{
+			sum = sum + c;
+		}
+
+		a = b;
+		b = c;
+	}
+	printf("%d\n", sum);
+
+	return (0);
+}

b4a80959d3f79d7464c008022b73727de553ea77
Stuart <108@holbertonschool.com>
2017-02-17 07:46:11 +0000

    101-print_number.c: initial commit

---
+++ 0x03-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * print_number - prints integers
+ *
+ *@n: the number to print
+ *
+ *Return: void
+ */
+
+void print_number(int n)
+{
+	int x, i;
+
+	i = 1;
+	if (n < 0)
+	{
+		n = n * -1;
+		_putchar('-');
+	}
+
+	for ( ; i * 10 <= n; i = i * 10)
+	{}
+
+	for ( ; i > 1 ; i = i / 10)
+	{
+		x = n / i;
+		_putchar(x + '0');
+		n = n - x * i;
+	}
+	_putchar(n + '0');
+}

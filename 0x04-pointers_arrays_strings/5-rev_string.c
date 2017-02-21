3da9cf086e3278b4a0100eb421f51b43808fdf85
Stuart <108@holbertonschool.com>
2017-02-21 22:30:59 +0000

    test higher char count

--- 0x04-pointers_arrays_strings/5-rev_string.c
+++ 0x04-pointers_arrays_strings/5-rev_string.c
@@ -1,5 +1,5 @@
 #include "holberton.h"
-
+#include <stdio.h>
 /**
  * rev_string - prints a string forwrd and backward
  *
@@ -11,7 +11,8 @@
 void rev_string(char *s)
 {
 	int i, j, len;
-	char s2[20];
+	char s2[64];
+
 
 	i = j = len = 0;
 	while (s[i] != 0)
@@ -33,4 +34,12 @@ void rev_string(char *s)
 		s[i] = s2[j];
 		j++;
 	}
+}
+
+int main(void)
+{
+	char s[100] = "hghghghghghghghghghghghghghghghghghghghghghghghghghg";
+	rev_string(s);
+	printf("%s\n", s);
+	return (0);
 }

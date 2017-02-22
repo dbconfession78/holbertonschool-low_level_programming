66f4674ad53f1c0aff47a447e66504da709a6e03
Stuart <108@holbertonschool.com>
2017-02-22 04:24:48 +0000

    revert

--- 0x04-pointers_arrays_strings/100-atoi.c
+++ 0x04-pointers_arrays_strings/100-atoi.c
@@ -1,44 +1,145 @@
 #include "holberton.h"
 #include <stdio.h>
+
+
 /**
- * _atoi - returns integer values from string
- * @s: input string
+ * _atoi - converts a string to an integer
  *
- * Return: will return integer
+ *@s: string to convert to int
+ *
+ * Return: string as an int
  */
+
 int _atoi(char *s)
 {
-	int result = 0, nth = 1, c = 0, digits = 0, start = 0, sign = 0;
+	int i, j, len, d, mult10, multiplier;
+	char x[_strlen(s)];
 
-	while (s[c] != 0)
+	d = j = 0;
+	multiplier = check_sign(s);
+	len = _strlen(s);
+	x[len] = '\0';
+	if (s[0] == 0)
+		return (0);
+	/* copy numeric chars into new strings */
+	for (i = 0; i < len; i++)
 	{
-		if (s[c] == '-' || s[c] == '+')
-			s[c] == '+' ? sign++ : sign--;
-		if (s[c] > 47 && s[c] < 58)
+		if (s[i] >= '0' && s[i] <= '9')
 		{
-			start += c;
-			while (s[c] > 47 && s[c] < 58 && s[c] != 0)
-				digits++, c++;
+			x[j] = s[i];
+			j++;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+		}
+	}
+	x[j] = '\0';
+	mult10 = 1;
+	for (i = 1; i < _strlen(x); i++)
+	{
+		mult10 *= 10;
+	}
+	/* convert the string to int */
+	for (i = 0; i < len; i++)
+	{
+		d = d + ((int) x[i] - 48) * mult10;
+		mult10 /= 10;
+	}
+	/* set sign */
+	d *= multiplier;
+	return (d);
+}
+
+/**
+ * _atoi - checks the sign of a number in a char string
+ *
+ *@str: string to check
+ *
+ * Return: 1 if positive; -1 if negative;
+ */
+
+int check_sign(char *str)
+{
+	int i, multiplier, len;
+
+
+	multiplier = 1;
+	len = _strlen(str);
+	for (i = 0; i < len; i++)
+	{
+		if (str[i] >= '0' && str[i] <= '9')
+		{
 			break;
 		}
-		c++;
-	}
-	if (digits > 0)
-	{
-		c--;
-		for (digits -= 1; digits > 0; digits--)
-			nth *= 10;
-		while (start <= c)
+		if (str[i] == '-')
 		{
-			if (sign >= 0)
-				result += (s[start] - 48) * nth;
+			if (multiplier == 1)
+			{
+				multiplier = -1;
+			}
 			else
-				result -= (s[start] - 48) * nth;
-			nth /= 10;
-			start++;
+			{
+				multiplier = 1;
+			}
 		}
-		return (result);
+		else if (str[i] == '+')
+		{
+			if (multiplier == -1)
+			{
+				multiplier = -1;
+			}
+			else
+			{
+				multiplier = 1;
+			}
+		}
 	}
-	else
-		return (0);
+	return multiplier;
+}
+
+/**
+ * _strlen - returns the length of a string
+ *
+ *@s: character array to count
+ *
+ * Return: number of characters
+ */
+
+int _strlen(char *s)
+{
+	int i, c;
+
+	i = 0;
+	c = -1;
+	while (c != 0)
+	{
+		c = s[i];
+		if (c != 0)
+		{
+			i++;
+		}
+	}
+	return (i);
+}
+
+int main(void)
+{
+    int n;
+
+    n = _atoi("98");
+    printf("%d\n", n);
+    n = _atoi("-402");
+    printf("%d\n", n);
+    n = _atoi("          ------++++++-----+++++--98");
+    printf("%d\n", n);
+    n = _atoi("214748364");
+    printf("%d\n", n);
+    n = _atoi("0");
+    printf("%d\n", n);
+    n = _atoi("Suite 402");
+    printf("%d\n", n);
+    n = _atoi("         +      +    -    -98 Battery Street; San Francisco, CA 94111 - USA             ");
+    printf("%d\n", n);
+    n = _atoi("---++++ -++ Sui - te -   402 #cisfun :)");
+    printf("%d\n", n);
+    return (0);
 }

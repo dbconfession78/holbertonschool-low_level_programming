af041a4d16d5fd8fced087057e420995b1a8555a
Stuart <108@holbertonschool.com>
2017-02-22 04:46:20 +0000

    fix

--- 0x04-pointers_arrays_strings/100-atoi.c
+++ 0x04-pointers_arrays_strings/100-atoi.c
@@ -21,7 +21,7 @@ int _atoi(char *s)
 	x[len] = '\0';
 	if (s[0] == 0)
 		return (0);
-	/* copy numeric chars into new strings */
+
 	for (i = 0; i < len; i++)
 	{
 		if (s[i] >= '0' && s[i] <= '9')
@@ -29,22 +29,26 @@ int _atoi(char *s)
 			x[j] = s[i];
 			j++;
 			if (s[i + 1] < '0' || s[i + 1] > '9')
+			{
 				break;
+			}
 		}
 	}
 	x[j] = '\0';
+	if (x[0] == '\0')
+	{
+		return (0);
+	}
 	mult10 = 1;
 	for (i = 1; i < _strlen(x); i++)
 	{
 		mult10 *= 10;
 	}
-	/* convert the string to int */
 	for (i = 0; i < len; i++)
 	{
 		d = d + ((int) x[i] - 48) * mult10;
 		mult10 /= 10;
 	}
-	/* set sign */
 	d *= multiplier;
 	return (d);
 }

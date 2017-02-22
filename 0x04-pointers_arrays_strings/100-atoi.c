7c180e6e1da3dcef0c95a5c14ddca21ec30f5928
Stuart <108@holbertonschool.com>
2017-02-22 05:58:19 +0000

    fix

--- 0x04-pointers_arrays_strings/100-atoi.c
+++ 0x04-pointers_arrays_strings/100-atoi.c
@@ -12,112 +12,47 @@
 
 int _atoi(char *s)
 {
-	int i, j, len, d, mult10, multiplier;
-	char x[_strlen(s)];
+	int result, sign, i, x, num_count, mult10;
+	result = 0, i = 0, sign = 0, x = 0, num_count = 0, mult10 = 1;
 
-	d = j = 0;
-	multiplier = check_sign(s);
-	len = _strlen(s);
-	x[len] = '\0';
-	if (s[0] == 0)
-		return (0);
-
-	for (i = 0; i < len; i++)
+	while (s[i] != '\0')
 	{
-		if (s[i] >= '0' && s[i] <= '9')
+		if (s[i] == '-' || s[i] == '+')
 		{
-			x[j] = s[i];
-			j++;
-			if (s[i + 1] < '0' || s[i + 1] > '9')
-			{
-				break;
-			}
+			s[i] == '+' ? sign++ : sign--;
 		}
-	}
-	x[j] = '\0';
-	if (x[0] == '\0')
-	{
-		return (0);
-	}
-	mult10 = 1;
-	for (i = 1; i < _strlen(x); i++)
-	{
-		mult10 *= 10;
-	}
-	for (i = 0; i < len; i++)
-	{
-		d = d + ((int) x[i] - 48) * mult10;
-		mult10 /= 10;
-	}
-	d *= multiplier;
-	return (d);
-}
 
-/**
- * check_sign - checks the sign of a number in a char string
- *
- *@str: string to check
- *
- * Return: 1 if positive; -1 if negative;
- */
-
-int check_sign(char *str)
-{
-	int i, multiplier, len;
-
-	multiplier = 1;
-	len = _strlen(str);
-	for (i = 0; i < len; i++)
-	{
-		if (str[i] >= '0' && str[i] <= '9')
+		if (s[i] >= 48  && s[i] <= 57)
 		{
+			x = x + i;
+			while (s[i] >= 48 && s[i] <= 57 && s[i] != '\0')
+			{
+				num_count++;
+				i++;
+			}
 			break;
 		}
-		if (str[i] == '-')
-		{
-			if (multiplier == 1)
-			{
-				multiplier = -1;
-			}
-			else
-			{
-				multiplier = 1;
-			}
-		}
-		else if (str[i] == '+')
-		{
-			if (multiplier == -1)
-			{
-				multiplier = -1;
-			}
-			else
-			{
-				multiplier = 1;
-			}
-		}
+		i++;
 	}
-	return (multiplier);
-}
-
-/**
- * _strlen - returns the length of a string
- *
- *@s: character array to count
- *
- * Return: number of characters
- */
-
-int _strlen(char *s)
-{
-	int i;
-
-	i = 0;
-	while (s[i] != 0)
+	if (num_count > 0)
 	{
-		if (s[i] != 0)
+		i = i - 1;
+		for (num_count = num_count - 1; num_count > 0; num_count--)
 		{
-			i++;
+			mult10 *= 10;
+		}
+		while (x <= i)
+		{
+			if (sign >= 0)
+			{
+				result += (s[x] - 48) * mult10;
+			}
+			else {
+				result -= (s[x] - 48) * mult10;
+			}
+			mult10 /= 10;
+			x++;
 		}
 	}
-	return (i);
+	return (result);
 }

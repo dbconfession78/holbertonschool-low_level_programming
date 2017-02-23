b85a65b9a40e2fd2083aba870ea47f11363d9fc1
Stuart <108@holbertonschool.com>
2017-02-23 03:06:47 +0000

    fix 100-atoi.c

--- 0x04-pointers_arrays_strings/100-atoi.c
+++ 0x04-pointers_arrays_strings/100-atoi.c
@@ -42,39 +42,3 @@ int _atoi(char *s)
 	}
 	return (result);
 }
-
-/*int main(void)
-{
-	int n;
-
-	n = _atoi("asdfass><><><.,.,.,.lajsfas]]ew[w[]we[j");
-	  printf("%d\n", n);
-	n = _atoi("-2147483648");
-	printf("%d\n", n);
-	n = _atoi("2147483647");
-    printf("%d\n", n);
-
-
-	n = _atoi("98");
-	printf("%d\n", n);
-	n = _atoi("-402");
-	printf("%d\n", n);
-	n = _atoi("          ------++++++-----+++++--98");
-	printf("%d\n", n);
-	n = _atoi("214748364");
-    printf("%d\n", n);
-	n = _atoi("0");
-	printf("%d\n", n);
-	n = _atoi("Suite 402");
-	printf("%d\n", n);
-	n = _atoi("         +      +    -    -98 Battery Street; San Fr\
-      ancisco, CA 94111 - USA             ");
-	printf("%d\n", n);
-	n = _atoi("---++++ -++ Sui - te -   402 #cisfun :)");
-	printf("%d\n", n);
-
-
-
-	return (0);
-
-	} */

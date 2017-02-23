10bd1e37e45e835e25dae399a2423cd5527e4e68
Stuart <108@holbertonschool.com>
2017-02-23 21:40:44 +0000

    add and use _strlen function

--- 0x05-pointers_arrays_strings/1-strncat.c
+++ 0x05-pointers_arrays_strings/1-strncat.c
@@ -10,21 +10,36 @@
  *
  * Return: concatenated string
  */
-
 char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0;
+	char end[n + 1];
+
+	for (i = 0; i < n; i++)
+		end[i] = src[i];
+	end[i] = '\0';
+	_strcat(dest, end);
+	return (dest);
+}
+
+/**
+ * _strcat - concatenates two strings together
+ *
+ * @dest: src gets appended to this
+ * @src: gets appended to dest
+ *
+ * Return: concatenated character array
+ */
+
+char *_strcat(char *dest, char *src)
 {
 	int i = 0, j = 0;
 
-	n > _strlen(src) ? n = _strlen(src) : n;
 	while (dest[i] != '\0')
 		i++;
-	for (j = 0; j != n ; j++)
-	{
+	for (j = 0; src[j] != '\0'; j++)
 		dest[i + j] = src[j];
-		if (src[j] == '\0')
-			break;
-	}
-	dest[i + n] = '\0';
 
+	dest[i + j] = '\0';
 	return (dest);
 }

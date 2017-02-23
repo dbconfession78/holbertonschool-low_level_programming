2e7b98d82bbb66e5bb76193944b757cd1faaff85
Stuart <108@holbertonschool.com>
2017-02-23 11:55:23 +0000

    add support files and unfinished project files

---
+++ 0x04-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * keygen - generates random password for the program 101-crackme
+ *
+ * @: ???
+ *
+ * Return: ???
+ */
+
+void keygen(void)
+{
+
+	printf("TEST_1!!!\n");
+	printf("BREAK_1!!!\n");
+	printf("TEST2!!!\n");
+	
+
+}
+
+/**
+ * main - entry point
+ *
+ *
+ *
+ * Return: always return 0
+ */
+
+/* gcc -Wall -pedantic -Werror -Wextra 101-keygen.c -o 101-keygen
+   julien@ubuntu:~/0x04$ ./101-crackme "`./101-keygen`" */
+
+int main(void)
+{
+	keygen();
+
+	return (0);
+}

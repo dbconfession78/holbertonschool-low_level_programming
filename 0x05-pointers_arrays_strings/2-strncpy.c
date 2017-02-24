e4b89a66671d11b0a2eb835f72bbc4eea36902ad
Stuart <108@holbertonschool.com>
2017-02-24 01:24:29 +0000

    fix

--- 0x05-pointers_arrays_strings/2-strncpy.c
+++ 0x05-pointers_arrays_strings/2-strncpy.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 
 /**
+ * _strncpy - copies src substring 0-# src range to destinto dest
  *
+ * @dest: empty char that src is copied into
+ * @src: 0-index ranged string copied into dest
+ * @n: src index to copy up to
  *
- *
- *
- * Return:
+ * Return: copied string
  */
 
 

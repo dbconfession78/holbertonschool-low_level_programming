68c58b66d7029630c522893fac40616dcccfbf4d
Stuart <108@holbertonschool.com>
2017-02-24 20:25:02 +0000

    fix

--- 0x05-pointers_arrays_strings/7-leet.c
+++ 0x05-pointers_arrays_strings/7-leet.c
@@ -10,19 +10,15 @@
 
 char *leet(char *s)
 {
-	int i, j;
-	char alpha[] = "aeotlAEOTL";
-	char nums[] = "4307143071";
 
-	for (i = 0; s[i] != '\0' ; i++)
+	int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (j = 0; alpha[j] != '\0'; j++)
+		for (j = 0; s[j] != '\0'; j++)
 		{
 			if (s[i] == alpha[j])
-			{
 				s[i] = nums[j];
-				break;
-			}
 		}
 	}
 

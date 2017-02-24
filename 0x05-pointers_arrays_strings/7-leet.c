d00498ec4c4ce6cfd2b1dee913d0acf6cd168e2f
Stuart <108@holbertonschool.com>
2017-02-24 20:11:38 +0000

    refactor 7-leet.c

--- 0x05-pointers_arrays_strings/7-leet.c
+++ 0x05-pointers_arrays_strings/7-leet.c
@@ -10,13 +10,13 @@
 
 char *leet(char *s)
 {
-	int i, j = 0;
+	int i, j;;
 	char alpha[11] = "aeotlAEOTL";
 	char nums[11] = "4307143071";
 
 	for (i = 0; s[i] != '\0' ; i++)
 	{
-		for (j = 0; j < _strlen(alpha); j++)
+		for (j = 0; alpha[j] != '\0')
 		{
 			if (s[i] == alpha[j])
 			{

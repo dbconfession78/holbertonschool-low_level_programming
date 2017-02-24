fb5225972f35567a9c011c2e1f2c3cc6f6ee1141
Stuart <108@holbertonschool.com>
2017-02-24 20:25:37 +0000

    fix

--- 0x05-pointers_arrays_strings/7-leet.c
+++ 0x05-pointers_arrays_strings/7-leet.c
@@ -15,7 +15,7 @@ char *leet(char *s)
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (j = 0; s[j] != '\0'; j++)
+		for (j = 0; alpha[j] != '\0'; j++)
 		{
 			if (s[i] == alpha[j])
 				s[i] = nums[j];

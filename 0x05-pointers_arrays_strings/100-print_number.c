65d9531a31342a56768a99f47ed9dd73ea600d94
Stuart <108@holbertonschool.com>
2017-02-26 20:18:30 +0000

    initial commit for 100-print_number.c

---
+++ 0x05-pointers_arrays_strings/100-print_number.c
@@ -0,0 +1,48 @@
+#include "holberton.h"
+#include <limits.h>
+#include <stdio.h>
+
+/**
+ *  print_number - prints an integer
+ *
+ * @n: integer to print
+ *
+ * Return: void
+ */
+
+void print_number(int n)
+{
+	int i, x, len, mult10,  last;
+
+	mult10 = 1;
+	len = last = 0;
+	if (n < 0)
+	{
+		_putchar('-');
+		if (n < 2147483647)
+		{
+			last = (n % 10) * -1;
+			n /= 10;
+		}
+		n *= -1;
+	}
+
+	x = n;
+	while (x != 0)
+	{
+		x /= 10;
+		if (x != 0)
+			mult10 *= 10;
+		len++;
+	}
+	x = n / mult10;
+	_putchar(x + '0');
+	for (i = 1; i < len; i++)
+	{
+		mult10 /= 10;
+		x = (n / mult10);
+		_putchar((x % 10) + '0');
+	}
+	if (last != 0)
+		_putchar(last + '0');
+}

91a005631a18df7f4c8d36fac4233e7feb966c8b
Stuart <108@holbertonschool.com>
2017-02-26 20:31:03 +0000

    fix

--- 0x05-pointers_arrays_strings/100-print_number.c
+++ 0x05-pointers_arrays_strings/100-print_number.c
@@ -14,29 +14,23 @@ void print_number(int n)
 
 	mult10 = 1;
 	len = last = 0;
-
-	if (n == 0)
-	{
-		_putchar(0 + '0');
-		return;
-	}
 	if (n < 0)
 	{
-		_putchar('-');
-		if (n < 2147483647)
+		if (n < -2147483647)
 		{
 			last = (n % 10) * -1;
 			n /= 10;
 		}
+		_putchar('-');
 		n *= -1;
 	}
-
 	x = n;
 	while (x != 0)
 	{
 		x /= 10;
 		if (x != 0)
 			mult10 *= 10;
+
 		len++;
 	}
 	x = n / mult10;
@@ -44,7 +38,7 @@ void print_number(int n)
 	for (i = 1; i < len; i++)
 	{
 		mult10 /= 10;
-		x = (n / mult10);
+		x = n / mult10;
 		_putchar((x % 10) + '0');
 	}
 	if (last != 0)

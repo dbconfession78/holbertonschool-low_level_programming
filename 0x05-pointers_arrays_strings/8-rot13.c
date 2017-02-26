25676d066ef79b03c90c4dbe8abc2fc4e4df4ba1
Stuart <108@holbertonschool.com>
2017-02-26 05:36:15 +0000

    try diff way

--- 0x05-pointers_arrays_strings/8-rot13.c
+++ 0x05-pointers_arrays_strings/8-rot13.c
@@ -1,28 +1,37 @@
 #include "holberton.h"
+#include <stdio.h>
 
 /**
  * rot13 - encodes a string using rot13
  *
- * @str: string to encrypt
+ * @s: string to encrypt
  *
  * Return: encrypted rot13 string
  */
 
-char *rot13(char *str)
+char *rot13(char *s)
 {
 	int i, j;
-	char *a1 = "abcdefghijklmABCDEFGHIJKLM";
-	char *a2 = "nopqrstuvwxyzNOPQRSTUVWXYZ";
 
-	for (i = 0; str[i] != 0; i++)
-		for (j = 0; j <= 25; j++)
+	char *cipher1 = "ABCDEFGHIJKLMNOPQRSTUVQXYZABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char *cipher2 = "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; cipher1[j] != '\0'; j++)
 		{
-			if (str[i] == a1[j])
-				str[i] = a2[j];
-			else if (str[i] == a2[j])
-				str[i] = a1[j];
-			else
-				str[i] = str[i];
+			if (s[i] == cipher1[j])
+			{
+				s[i] = cipher1[j + 13];
+				break;
+			}
+			else if (s[i] == cipher2[j])
+			{
+				s[i] = cipher2[j + 13];
+				break;
+			}
 		}
-	return (str);
+	}
+
+	return (s);
 }

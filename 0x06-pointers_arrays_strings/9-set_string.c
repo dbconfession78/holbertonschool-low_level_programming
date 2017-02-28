c4c26e4303443c5c8afab940e901affe41ff8bd0
Stuart <108@holbertonschool.com>
2017-02-28 07:04:38 +0000

    init commit: 9-set_string.c

---
+++ 0x06-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * set_string - sets the value of a pointer to a char
+ *
+ *@s: pointer to 'to'
+ *@to: pointer
+ *
+ * Return: void
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}

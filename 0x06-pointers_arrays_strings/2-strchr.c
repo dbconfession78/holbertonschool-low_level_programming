558fd6397240ee89df0ab0764c1ad8f9a6b9ac29
Stuart <108@holbertonschool.com>
2017-02-28 23:33:01 +0000

    refactor

--- 0x06-pointers_arrays_strings/2-strchr.c
+++ 0x06-pointers_arrays_strings/2-strchr.c
@@ -17,16 +17,9 @@ char *_strchr(char *s, char c)
 	{
 		if (s[i] == c)
 		{
-			/*return (s + i); */
-			break;
+			return (&s[i]);
 		}
 	}
-	if (s[i] == c)
-	{
-		return (s + i);
-	}
-	else
-	{
-		return (0);
-	}
+
+	return (0);
 }

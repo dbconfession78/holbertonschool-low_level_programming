e032d38d8a2b2df194a85c08d70e7fc3c5db723c
Stuart <108@holbertonschool.com>
2017-03-01 20:15:53 +0000

    init commit: 1-print_rev_recursion.c

---
+++ 0x07-recursion/1-print_rev_recursion.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * _print_rev_recursion - prints a string in reverse
+ *
+ *@s: string to print
+ *
+ * Return: void
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_print_rev_recursion(s+1);
+		_putchar(*s);
+	}
+}
+
+int main(void)
+{
+    _print_rev_recursion("\nColton Walker");
+    return (0);
+}

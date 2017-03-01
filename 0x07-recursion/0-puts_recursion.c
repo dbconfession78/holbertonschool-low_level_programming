265a44422fc2fc34e9f5cb2e1c728a6e9a41b279
Stuart <108@holbertonschool.com>
2017-03-01 20:50:31 +0000

    fix

--- 0x07-recursion/0-puts_recursion.c
+++ 0x07-recursion/0-puts_recursion.c
@@ -11,18 +11,10 @@
 
 void _puts_recursion(char *s)
 {
-
-	if (s == "" || s == "\0")
-		return;
-
-	if (!*s)
-		_putchar('\n');
-
-	else
+	if (*s)
 	{
-		_putchar(s[0]);
-		*s++;
-		_puts_recursion(s);
+		putchar(*s);
+		_puts_recursion(s + 1);
+		
 	}
-
 }

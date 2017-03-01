6fefe8085c0e2ac7cb05d224ec70b19739aab97f
Stuart <108@holbertonschool.com>
2017-03-01 20:18:16 +0000

    remove main function

--- 0x07-recursion/1-print_rev_recursion.c
+++ 0x07-recursion/1-print_rev_recursion.c
@@ -13,13 +13,7 @@ void _print_rev_recursion(char *s)
 {
 	if (*s)
 	{
-		_print_rev_recursion(s+1);
+		_print_rev_recursion(s + 1);
 		_putchar(*s);
 	}
-}
-
-int main(void)
-{
-    _print_rev_recursion("\nColton Walker");
-    return (0);
 }

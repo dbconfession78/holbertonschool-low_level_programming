10f5ffcc1e7050208ed307ebc611b33a2bf3ea38
Stuart <108@holbertonschool.com>
2017-03-01 21:24:48 +0000

    switch back to _putchar for newline

--- 0x07-recursion/0-puts_recursion.c
+++ 0x07-recursion/0-puts_recursion.c
@@ -11,11 +11,11 @@
 
 void _puts_recursion(char *s)
 {
-	if (*s)
-	{
-		putchar(*s);
+
+	if (!*s)
+		_putchar('\n');
+	else {
+		_putchar(*s);
 		_puts_recursion(s + 1);
 	}
-	else
-		printf("\n");
 }

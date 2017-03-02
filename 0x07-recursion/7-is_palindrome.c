eea7c5e77872634debb426f0adfd535b1a1a909e
Stuart <108@holbertonschool.com>
2017-03-02 03:44:44 +0000

    add _strlen_recursion helper function to file

--- 0x07-recursion/7-is_palindrome.c
+++ 0x07-recursion/7-is_palindrome.c
@@ -11,7 +11,7 @@
 
 int is_palindrome(char *s)
 {
-	return (my_is_palindrome(s, _strlen(s)));
+	return (my_is_palindrome(s, _strlen_recursion(s)));
 }
 
 /**
@@ -34,4 +34,22 @@ int my_is_palindrome(char *s, int len)
 	}
 	else
 		return (0);
+}
+
+/**
+ * _strlen_recursion - returns the length of a string
+ *
+ *@s: string to return length of
+ *
+ * Return: length of string
+ */
+
+int _strlen_recursion(char *s)
+{
+	s++;
+	if (s[-1] != '\0')
+	{
+		return (_strlen_recursion(s) + 1);
+	}
+	return (0);
 }

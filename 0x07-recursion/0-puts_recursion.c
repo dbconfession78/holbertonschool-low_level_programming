5d512280d81e6de83d3aa8c1dcc387994377f334
Stuart <108@holbertonschool.com>
2017-03-02 20:50:11 +0000

    refactor

--- 0x07-recursion/0-puts_recursion.c
+++ 0x07-recursion/0-puts_recursion.c
@@ -11,11 +11,11 @@
 
 void _puts_recursion(char *s)
 {
-	if (!*s))
+	if (!*s)
 		_putchar('\n');
 	else
 	{
-		_putchar(*s++);
-		_puts_recursion(s);
+		_putchar(*s);
+		_puts_recursion(s + 1);
 	}
 }

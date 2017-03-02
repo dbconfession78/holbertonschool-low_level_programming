bfc719b3ec789f1e8fadbaa385bf98c70694423e
Stuart <108@holbertonschool.com>
2017-03-02 01:49:27 +0000

    init commit: 5-sqrt_recursion.c and holberton.h update

---
+++ 0x07-recursion/5-sqrt_recursion.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * _sqrt_recursion - returns natural square root of a number
+ *
+ * @n: number to find natural square root of
+ *
+ * Return: natural square root of n
+ */
+
+int _sqrt_recursion(int n)
+{
+	return (my_sqrt(n, 1));
+}
+
+
+/**
+ * my_sqrt - checks if square root exists
+ * 
+ * @n: number to check
+ * @mult: incrementally squared to match with n
+ *
+ * Return: square root if found; -1 if not found
+ */
+
+int my_sqrt(int n, int mult)
+{
+	int square = mult * mult;
+
+	if (square > n)
+		return (-1);
+	if (square == n)
+		return (mult);
+	else
+		return (my_sqrt(n, mult + 1));
+}

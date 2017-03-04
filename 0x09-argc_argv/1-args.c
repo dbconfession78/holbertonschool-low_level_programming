8dd50fddc334fc0c7d9cddde37da916a673e5949
Stuart <108@holbertonschool.com>
2017-03-04 04:58:43 +0000

    1-args.c

---
+++ 0x09-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * main - entry point
+ *
+ * @argc: argument count
+ * @argv: arguments passed to program
+ *
+ * Return: always returns 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argv;
+	printf("%d\n", argc);
+	return (0);
+}

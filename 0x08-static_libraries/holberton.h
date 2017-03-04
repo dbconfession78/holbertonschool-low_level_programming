017670b5f7f8e1cc595ac41e8eb4415e8e4b0fed
Stuart <sgkur04@gmail.com>
2017-03-04 22:40:52 +0000

    add #endif to end of holberton.h

--- 0x08-static_libraries/holberton.h
+++ 0x08-static_libraries/holberton.h
@@ -1,4 +1,4 @@
-#ifndef HOLBERTON_H
+#ifndefine HOLBERTON_H
 #define HOLBERTON_H
 int _putchar(char c);
 int _islower(int c);
@@ -20,3 +20,4 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+#endif

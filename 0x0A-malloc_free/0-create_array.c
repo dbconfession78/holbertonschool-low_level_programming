3e995b173184b0cf5614194b49407e10401f5c20
Stuart <sgkur04@gmail.com>
2017-03-07 21:02:16 +0000

    add error handler for case: size = 0;

--- 0x0A-malloc_free/0-create_array.c
+++ 0x0A-malloc_free/0-create_array.c
@@ -18,6 +18,8 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 	char *str = malloc(size * sizeof(char));
 
+	if (size == 0)
+		return (NULL);
 	for (i = 0; i < size; i++)
 	{
 		str[i] = c;

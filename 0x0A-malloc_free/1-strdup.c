4b9481ef919d14fa90ab2ad15ac8136f91457763
Stuart <sgkur04@gmail.com>
2017-03-07 21:28:55 +0000

    put parenthesis around NULL return value;

--- 0x0A-malloc_free/1-strdup.c
+++ 0x0A-malloc_free/1-strdup.c
@@ -7,7 +7,7 @@
 
 /**
  * _strdup - returns a pointer to a newly allocated space in mem
- * which contains a copy of string gievn as a param
+ * which contains a copy of string givem as a param
  *
  * @str: string copied into mem location being pointed to
  *
@@ -16,14 +16,18 @@
 
 char *_strdup(char *str)
 {
-
+	unsigned int i;
 	char *str2 = malloc((strlen(str) * sizeof(char)));
 	char *p_str2 = &str2[0];
 
 	if (str == NULL)
-		return NULL;
+		return (NULL);
 
-	strcpy(str2, str);
+/*	strcpy(str2, str); */
+	for (i = 0; i < strlen(str); i++)
+	{
+		str2[i] = str[i];
+	}
 
 	return (p_str2);
 }

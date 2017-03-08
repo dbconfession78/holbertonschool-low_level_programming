a5f01a429f5c04fe8b6e032d1814d1fa6e67eaf2
Stuart <sgkur04@gmail.com>
2017-03-08 04:14:36 +0000

    add +1 to tot_len counter

--- 0x0A-malloc_free/5-argstostr.c
+++ 0x0A-malloc_free/5-argstostr.c
@@ -1,58 +1,67 @@
-#include "holberton.h"
+#include <stdio.h>
 #include <stdlib.h>
+#include "holberton.h"
+
 /**
- * _strlen - returns the lenght of a string
- * @s: pointer to s
+ * _strlen - returns the length of a string
  *
- * Return: 0 on success
+ * @s: character array to count
  *
+ * Return: number of characters
  */
+
 int _strlen(char *s)
 {
-	int count = 0;
+	int i, c;
 
-	if (s != '\0')
+	i = 0;
+	c = -1;
+	while (c != 0)
 	{
-		while (*(s + count) != '\0')
-			count++;
+		c = s[i];
+		if (c != 0)
+		{
+			i++;
+		}
 	}
-	return (count);
+	return (i);
 }
+
 /**
- * argstostr - concatenates all the arguments of your program
- * @ac: number of args
- * @av: arguments
+ * argstostr - concatenates all arguments passed to function
  *
- * Return: pointer to new string
+ * @ac: argument count
+ * @av: argument vectors
+ *
+ * Return: concatenated string
  */
+
 char *argstostr(int ac, char **av)
 {
-	int i, j, k, len, total_len;
-	char *new_str;
+	int i, j, k = 0, tot_len = 0;
+	char *result;
 
 	if (ac == 0 || av == NULL)
 		return (NULL);
-	len = 0;
-	total_len = 0;
+
+	/* get tot len */
 	for (i = 0; i < ac; i++)
-	{
-		len = _strlen(av[i]);
-		total_len += len + 1;
-	}
-	new_str = malloc(sizeof(char) * (total_len + 1));
-	if (new_str == NULL)
+		tot_len += _strlen(av[i]) + 1;
+
+	result = malloc(sizeof(char) * (tot_len));
+	if (result == NULL)
 		return (NULL);
-	k = 0;
+
 	for (i = 0; i < ac; i++)
 	{
 		for (j = 0; av[i][j] != '\0'; j++)
 		{
-			new_str[k] = av[i][j];
+			result[k] = av[i][j];
 			k++;
 		}
-		new_str[k] = '\n';
+		result[k] = '\n';
 		k++;
 	}
-	new_str[k] = '\0';
-	return (new_str);
+	result[k] = '\0';
+	return (result);
 }

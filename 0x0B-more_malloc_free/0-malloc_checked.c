cf9c127e8b26528b6ea44cedf79f4cb11652c46f
Stuart <sgkur04@gmail.com>
2017-03-09 20:38:32 +0000

    init commit: 0-malloc_checked.c

---
+++ 0x0B-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,25 @@
+#include <stdio.h> /* remove before commiting */
+#include <stdlib.h>
+#include "holberton.h"
+#include <limits.h>
+
+/**
+ * malloc_checked - allocates memory using malloc
+ *
+ * @b: amount of memory to allocate
+ *
+ * Return: void
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *c = malloc(sizeof(unsigned int *) * b);
+
+	if (c == NULL)
+	{
+		return (c);
+		exit(98);
+	}
+
+	return (c);
+}

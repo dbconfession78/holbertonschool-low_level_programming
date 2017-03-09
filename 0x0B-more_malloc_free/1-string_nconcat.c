1ae62448228563f49b6e3ee5e200deb28e0f8591
Stuart <sgkur04@gmail.com>
2017-03-09 22:49:30 +0000

    revert to using pointer + len position for len counters

--- 0x0B-more_malloc_free/1-string_nconcat.c
+++ 0x0B-more_malloc_free/1-string_nconcat.c
@@ -46,13 +46,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-/*	for ( ; *(s1 + len1) != '\0'; len1++)
- *		;
- *	for ( ; *(s2 + len2) != '\0'; len2++)
- *	;
-*/
-	len1 = _strlen(s1) + 1;
-	len2 = _strlen(s2) + 1;
+	for ( ; *(s1 + len1) != '\0'; len1++)
+		;
+	for ( ; *(s2 + len2) != '\0'; len2++)
+		;
 	if (n >= len2)
 		n = len2;
 	tot_len = len1 + n + 1;

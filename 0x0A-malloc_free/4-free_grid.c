7757feb86b318b5feca2aab032902317c6402208
Stuart <108@holbertonschool.com>
2017-03-09 06:14:49 +0000

    put if NULL statement back

--- 0x0A-malloc_free/4-free_grid.c
+++ 0x0A-malloc_free/4-free_grid.c
@@ -18,7 +18,7 @@ void free_grid(int **grid, int height)
 	if (grid == NULL || height == 0)
 		return;
 	for (i = 0; i < height; i++)
-		/*if (grid[i] != NULL) */
+		if (grid[i] != NULL)
 			free(grid[i]);
 	free(grid);
 }

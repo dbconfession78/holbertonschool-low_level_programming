569a4206ea23337c6d99a70529be0aba83905895
Stuart <108@holbertonschool.com>
2017-03-09 05:55:39 +0000

    add 100-change.c

---
+++ 0x09-argc_argv/100-change.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * @argc: argument count
+ * @argv: arguments passed to program
+ *
+ * Return: always returns 0
+ */
+
+int main(int argc, char *argv[])
+{
+	return (0);
+}

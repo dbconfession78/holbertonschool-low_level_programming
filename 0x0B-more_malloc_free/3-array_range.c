413f62207f32a8bbc35d2cf29f987eb1f4a58a93
Stuart <sgkur04@gmail.com>
2017-03-10 01:38:10 +0000

    add edge handler for range == 0

--- 0x0B-more_malloc_free/3-array_range.c
+++ 0x0B-more_malloc_free/3-array_range.c
@@ -13,19 +13,23 @@
 int *array_range(int min, int max)
 {
 	int *array;
-	int i;
-	int range;
+	int i, range;
 
 	if (min > max)
 		return (NULL);
-
 	range = max - min;
-	array = malloc(sizeof(int) * range + 1);
-	if (!array)
-		return (NULL);
-	for (i = 0; i <= range; i++)
-		array[i] = min++;
-
+	if (range == 0)
+	{
+		array = malloc(sizeof(int));
+		*array = min;
+	}
+	else
+	{
+		array = malloc(sizeof(int) * (range + 1));
+		if (!array)
+			return (NULL);
+		for (i = 0; i <= max; i++)
+			array[i] = min++;
+	}
 	return (array);
-
 }

40080dc7724db865655aa9c06be1e56abcaac177
Stuart <sgkur04@gmail.com>
2017-03-10 05:08:20 +0000

    modify _memcpy loop condition so continues only if i < size of dest and i < size of src

--- 0x0B-more_malloc_free/100-realloc.c
+++ 0x0B-more_malloc_free/100-realloc.c
@@ -16,7 +16,7 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
 
-	for (i = 0; i < n; i++)
+	for (i = 0; i < sizeof(dest) && i < sizeof(src); i++)
 	{
 		dest[i] = src[i];
 	}

72d0d61c70c30d2751a2f0db56e3ec092091cddb
Stuart <sgkur04@gmail.com>
2017-03-10 01:04:31 +0000

    passed: cleanup; remove stray if (size == 0)

--- 0x0B-more_malloc_free/2-calloc.c
+++ 0x0B-more_malloc_free/2-calloc.c
@@ -18,15 +18,11 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-
 	array = malloc(nmemb * size);
-	if (array == NULL)
+	if (!array)
 		return (NULL);
-	if (array == 0)
-
 	for (i = 0; *(array + 1) != '\0'; i++)
 		array[i] = 0;
-
 	return (array);
 
 }

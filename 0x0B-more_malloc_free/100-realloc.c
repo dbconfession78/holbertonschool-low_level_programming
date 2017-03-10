0fd66a969be12f560786e120f39d548adc8d2d8a
Stuart <sgkur04@gmail.com>
2017-03-10 05:57:03 +0000

    passed: clean up

--- 0x0B-more_malloc_free/100-realloc.c
+++ 0x0B-more_malloc_free/100-realloc.c
@@ -18,7 +18,6 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 
 	if (!ptr)
 		return (malloc(new_size));
-
 	if (new_size == 0)
 	{
 		free(ptr);
@@ -26,14 +25,11 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	}
 	if (new_size == old_size)
 		return (ptr);
-
 	new_ptr = malloc(new_size);
 	if (!new_ptr)
 		return (NULL);
-
 	for (i = 0; i < old_size && i < new_size; i++)
 		new_ptr[i] = ((char *)ptr)[i];
 	free(ptr);
-
 	return (new_ptr);
 }

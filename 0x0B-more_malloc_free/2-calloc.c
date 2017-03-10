06578854de21b2f5fdbdae1d0fe574a01022f392
Stuart <sgkur04@gmail.com>
2017-03-10 01:02:54 +0000

    add cedge case size == 0

--- 0x0B-more_malloc_free/2-calloc.c
+++ 0x0B-more_malloc_free/2-calloc.c
@@ -16,12 +16,13 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	int i;
 	char *array;
 
-	if (nmemb == 0)
+	if (nmemb == 0 || size == 0)
 		return (NULL);
 
 	array = malloc(nmemb * size);
 	if (array == NULL)
 		return (NULL);
+	if (array == 0)
 
 	for (i = 0; *(array + 1) != '\0'; i++)
 		array[i] = 0;

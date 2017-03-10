ebc25daab0403246f5863f94f34c76b276aaea48
Stuart <sgkur04@gmail.com>
2017-03-10 05:45:20 +0000

    passed: refactoring

--- 0x0B-more_malloc_free/100-realloc.c
+++ 0x0B-more_malloc_free/100-realloc.c
@@ -18,8 +18,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 
 	if (!ptr)
 	{
-		new_ptr = malloc(new_size);
-		return (new_ptr);
+		return (malloc(new_size));
 	}
 
 	if (new_size == 0)

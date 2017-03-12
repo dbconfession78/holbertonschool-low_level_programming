50273060375da33a73c313d661aeab60981d1bfb
Stuart <108@holbertonschool.com>
2017-03-12 13:14:52 +0000

    temporarily remove main*.c from .gitignore

---
+++ 0x0C-preprocessor/2-main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
+
+
+/**
+ * main - entry point
+ *
+ *
+ *
+ * Return: always return 0
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}

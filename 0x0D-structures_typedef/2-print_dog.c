cd185874d0ddbcb81cd03418d2de699fe16fd7c9
Stuart <108@holbertonschool.com>
2017-03-12 16:26:27 +0000

    init push: 2-print_dog.c, add prototype for print_dog to dog.h

---
+++ 0x0D-structures_typedef/2-print_dog.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * print_dog - prints an instance of struct dog
+ *
+ * @dog: instance of struct dog
+ *
+ * Return: void
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		return;
+
+	printf("Name: ");
+	if (d->name != NULL)
+		printf("%s\n", d->name);
+	else
+		printf("(nil)\n");
+
+	printf("Age: %f\n", d->age);
+
+	printf("Owner: ");
+	if (d->owner != NULL)
+		printf("%s\n", d->owner);
+	else
+		printf("(nil)\n");
+
+}

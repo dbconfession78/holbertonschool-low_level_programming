178e0582812e307c8a1f81ffd00545792b9ff8cb
Stuart <108@holbertonschool.com>
2017-03-13 01:26:51 +0000

    test Naomi's code

--- 0x0E-function_pointers/3-main.c
+++ 0x0E-function_pointers/3-main.c
@@ -19,6 +19,17 @@ int main(int argc, char *argv[])
 	}
 	/* fun = get_op_func(argv[2]); */
 
+	if (
+		strcmp(argv[2], "+") != 0 &&
+		strcmp(argv[2], "-") != 0 &&
+		strcmp(argv[2], "*") != 0 &&
+		strcmp(argv[2], "/") != 0 &&
+		strcmp(argv[2], "%") != 0)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
 	if (((strcmp(argv[2], "/") == 0) || (strcmp(argv[2], "%") == 0)) &&
 		(argv[3] == 0))
 	{

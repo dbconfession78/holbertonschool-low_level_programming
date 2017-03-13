6b03d80a093477ca58c787d94c92ae6309a8fcf4
Stuart <108@holbertonschool.com>
2017-03-13 01:34:01 +0000

    test NAOMI code

--- 0x0E-function_pointers/3-main.c
+++ 0x0E-function_pointers/3-main.c
@@ -11,12 +11,13 @@
 int main(int argc, char *argv[])
 {
 	/* int (*fun)(int, int); */
-
+	int a, b;
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
+	a = atoi(argv[1]); b = atoi(argv[3]);
 	/* fun = get_op_func(argv[2]); */
 
 	if (
@@ -31,7 +32,7 @@ int main(int argc, char *argv[])
 	}
 
 	if (((strcmp(argv[2], "/") == 0) || (strcmp(argv[2], "%") == 0)) &&
-		(argv[3] == 0))
+		(b == 0))
 	{
 		printf("Error\n");
 		exit(100);
@@ -44,6 +45,6 @@ int main(int argc, char *argv[])
  *		}/
 */
 /*	printf("%d\n", fun(atoi(argv[1]), atoi(argv[3]))); */
-	printf("%d\n", get_op_func(argv[2])(atoi(argv[1]), atoi(argv[3])));
+	printf("%d\n", get_op_func(argv[2])(a, b));
 	return (0);
 }

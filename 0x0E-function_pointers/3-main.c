65f214e4b135be98136ba66592b1b27213df7ab1
Stuart <108@holbertonschool.com>
2017-03-13 01:45:55 +0000

    test NAOMI code

--- 0x0E-function_pointers/3-main.c
+++ 0x0E-function_pointers/3-main.c
@@ -1,22 +1,17 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include "3-calc.h"
-
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 /**
- * main - entry point for program that performs simple math
- *
- * @argc: number of arguments passed to program
- * @argv: arguments passed to program
- *
- * Return:0 if succesful, exit 98 if incorrect argument count,
- * exit 99 if incorrect operator supplied
+ * main - function to return arguments
+ * @argc: received argument count
+ * @argv: arguments provided at command line
+ * Return: void
  */
-
 int main(int argc, char *argv[])
 {
-	int a;
-	int b;
+	/* int (*fun)(int, int); */
+	int a, b;
 
 	if (argc != 4)
 	{
@@ -24,6 +19,8 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 	a = atoi(argv[1]); b = atoi(argv[3]);
+	/* fun = get_op_func(argv[2]); */
+
 	if (
 		strcmp(argv[2], "+") != 0 &&
 		strcmp(argv[2], "-") != 0 &&
@@ -34,12 +31,21 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(99);
 	}
+
 	if (((strcmp(argv[2], "/") == 0) || (strcmp(argv[2], "%") == 0)) &&
 		(b == 0))
 	{
 		printf("Error\n");
 		exit(100);
 	}
+
+/*	if (fun == NULL)
+ *	{
+ *		printf("Error\n");
+ *		exit(99);
+ *		}/
+*/
+/*	printf("%d\n", fun(atoi(argv[1]), atoi(argv[3]))); */
 	printf("%d\n", get_op_func(argv[2])(a, b));
 	return (0);
 }

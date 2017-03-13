48b80bd9a7e5116d6a1e1fc0b27e545c53235c11
Stuart <108@holbertonschool.com>
2017-03-13 01:25:09 +0000

    test Naomi's code

--- 0x0E-function_pointers/3-main.c
+++ 0x0E-function_pointers/3-main.c
@@ -1,45 +1,38 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include "3-calc.h"
-
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 /**
- * main - entry point for program that performs simple math
- *
- * @argc: number of arguments passed to program
- * @argv: arguments passed to prgoram
- *
- * Return:0 if succesful, exit 98 if incorrect argument count,
- * exit 99 if incorrect operator supplied
+ * main - function to return arguments
+ * @argc: received argument count
+ * @argv: arguments provided at command line
+ * Return: void
  */
-
 int main(int argc, char *argv[])
 {
-	int a;
-	int b;
+	/* int (*fun)(int, int); */
 
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	a = atoi(argv[1]); b = atoi(argv[3]);
-	if (
-		strcmp(argv[2], "+") != 0 &&
-		strcmp(argv[2], "-") != 0 &&
-		strcmp(argv[2], "*") != 0 &&
-		strcmp(argv[2], "/") != 0 &&
-		strcmp(argv[2], "%") != 0)
-	{
-		printf("Error\n");
-		exit(99);
-	}
+	/* fun = get_op_func(argv[2]); */
+
 	if (((strcmp(argv[2], "/") == 0) || (strcmp(argv[2], "%") == 0)) &&
-		(b == 0))
+		(argv[3] == 0))
 	{
 		printf("Error\n");
 		exit(100);
 	}
-	printf("%d\n", get_op_func(argv[2])(a, b));
+
+/*	if (fun == NULL)
+ *	{
+ *		printf("Error\n");
+ *		exit(99);
+ *		}/
+*/
+/*	printf("%d\n", fun(atoi(argv[1]), atoi(argv[3]))); */
+	printf("%d\n", get_op_func(argv[2])(atoi(argv[1]), atoi(argv[3])));
 	return (0);
 }

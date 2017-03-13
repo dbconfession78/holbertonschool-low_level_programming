ace306b4fad5be1b2d74d10b38751b3b5adb9371
Stuart <108@holbertonschool.com>
2017-03-13 03:48:08 +0000

    revert working copy

--- 0x0E-function_pointers/3-get_op_func.c
+++ 0x0E-function_pointers/3-get_op_func.c
@@ -1,6 +1,3 @@
-#include <string.h>
-#include "3-calc.h"
-
 /**
  * get_op_func - selects function to perform requested math operation
  *
@@ -9,6 +6,37 @@
  * Return: pointer to func corresponding to op given as aparm
  */
 
+#include "3-calc.h"
+#include <stdio.h>
+#include <string.h>
+/*
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i = 0;
+
+	while(i < 5)
+	{
+		if (strcmp(ops[i].op, s) == 0)
+		{
+			return (ops[i].f);
+		}
+		else
+		{
+			i++;
+		}
+		printf("Error\n");
+	}
+	return (0);
+}
+*/
 int (*get_op_func(char *s))(int, int)
 {
 	int i = 0;

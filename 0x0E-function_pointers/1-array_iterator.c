a8206cb557750d6039337afdf9e5d1170e662062
Stuart <sgkur04@gmail.com>
2017-03-15 19:36:58 +0000

    add size 0 condition handler

--- 0x0E-function_pointers/1-array_iterator.c
+++ 0x0E-function_pointers/1-array_iterator.c
@@ -18,7 +18,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i;
 
-	if(!array || !action)
+	if(!array || !action || size == 0)
 		return;
 
 	for (i = 0; i < size; i++)

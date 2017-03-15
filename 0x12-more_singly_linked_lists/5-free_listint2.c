f00d736e068b55a14c2043703b1174685dbaa8dc
Stuart <108@holbertonschool.com>
2017-03-15 21:24:16 +0000

    5-free_listint2.c: initial commit

---
+++ 0x12-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * free_listint2 -  ree a listint_t list
+ * @head: list to free
+ * Return: void
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *temp_node;
+
+	if (!*head)
+		return;
+	temp_node = *head;
+	while(*head)
+	{
+		temp_node = *head;
+		*head = temp_node->next;
+		free(temp_node);
+	}
+}

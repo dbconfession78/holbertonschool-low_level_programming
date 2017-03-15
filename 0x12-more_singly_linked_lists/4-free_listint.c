c770f3df7b5bd867d3a9944a568f1c8a8c7249e7
Stuart <108@holbertonschool.com>
2017-03-15 20:56:28 +0000

    correct memory leak

--- 0x12-more_singly_linked_lists/4-free_listint.c
+++ 0x12-more_singly_linked_lists/4-free_listint.c
@@ -9,16 +9,12 @@
 void free_listint(listint_t *head)
 {
 	listint_t *temp_node;
-	int *n_ptr;
 
-	if (!head)
-		return;
 	temp_node = head;
-	while (temp_node->n)
+	while(head)
 	{
-		n_ptr = &temp_node->n;
-		free(n_ptr);
-		if (temp_node->next)
-			temp_node = temp_node->next;
+		temp_node = head;
+		head = head->next;
+		free(temp_node);
 	}
 }

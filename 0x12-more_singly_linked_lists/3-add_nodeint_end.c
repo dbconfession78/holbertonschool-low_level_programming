2eb1885e77cf59bdb008ce0a0836baf1350f7a23
Stuart <108@holbertonschool.com>
2017-03-16 01:25:23 +0000

    3-add_nodeint_end.c: style compliance fix

--- 0x12-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x12-more_singly_linked_lists/3-add_nodeint_end.c
@@ -21,7 +21,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	else
 	{
 		temp_node = *head;
-		while(temp_node->next)
+		while (temp_node->next)
 			temp_node = temp_node->next;
 		temp_node->next = new_node;
 	}

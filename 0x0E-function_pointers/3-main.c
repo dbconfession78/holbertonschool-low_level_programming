ce3b4172a00aab4f1d0d648bd2c549f823320b19
Stuart <sgkur04@gmail.com>
2017-03-16 03:42:01 +0000

    revert to passed ver of 3-main.c after testing Elaine's

--- 0x0E-function_pointers/3-main.c
+++ 0x0E-function_pointers/3-main.c
@@ -1,39 +1,45 @@
-#include "3-calc.h"
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "3-calc.h"
+
 /**
- * main - entry point
- * @argc: argument count
- * @argv: argumemnt vector
- * Return: 0
+ * main - entry point for program that performs simple math
+ *
+ * @argc: number of arguments passed to program
+ * @argv: arguments passed to prgoram
+ *
+ * Return:0 if succesful, exit 98 if incorrect argument count,
+ * exit 99 if incorrect operator supplied
  */
 
 int main(int argc, char *argv[])
 {
 	int a;
 	int b;
-	int (*op)(int a, int b);
 
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	if (argv[2][1] != '\0')
+	a = atoi(argv[1]); b = atoi(argv[3]);
+	if (
+		strcmp(argv[2], "+") != 0 &&
+		strcmp(argv[2], "-") != 0 &&
+		strcmp(argv[2], "*") != 0 &&
+		strcmp(argv[2], "/") != 0 &&
+		strcmp(argv[2], "%") != 0)
 	{
 		printf("Error\n");
 		exit(99);
 	}
-	op = get_op_func(argv[2]);
-	if (op == NULL)
+	if (((strcmp(argv[2], "/") == 0) || (strcmp(argv[2], "%") == 0)) &&
+		(b == 0))
 	{
 		printf("Error\n");
-		exit(99);
+		exit(100);
 	}
-
-	a = atoi(argv[1]);
-	b = atoi(argv[3]);
-
-	printf("%d\n", op(a, b));
+	printf("%d\n", get_op_func(argv[2])(a, b));
 	return (0);
 }

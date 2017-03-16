93248a00beab46030818066d290686e2bfd83005
Stuart <108@holbertonschool.com>
2017-03-16 23:32:29 +0000

    solve with bitwise operators

--- 0x13-bit_manipulation/0-binary_to_uint.c
+++ 0x13-bit_manipulation/0-binary_to_uint.c
@@ -9,44 +9,18 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int len;
-	unsigned int digit;
-	unsigned int bin;
-	unsigned int result;
-	unsigned int pow;
-	unsigned int i = 0;
-	unsigned int sum = 0;
+	unsigned int i, result;
+	unsigned int bin_one = 1;
 
-	len = strlen(b);
-
-	/* check if b is NULL or any char is not 0 or 1 */
-	for (i = 1; i < len; i++)
+	if (!b)
+		return (0);
+	for (i = 0; b[i] != '\0'; i++)
 	{
-		if (!b || (b[i] != '1' && b[i] != '0'))
-		{
+		if (b[i] != '0' && b[i] != '1')
 			return (0);
-		}
+		result = result << 1;
+		if (b[i] == '1')
+			result = result ^ bin_one;
 	}
-	i = 0;
-	bin = atoi(b);
-	sum = bin % 10;
-	bin = bin / 10;
-	while (i < len)
-	{
-		digit = bin % 10;
-		result = 1;
-		if (digit == 1)
-		{
-			pow = i + 1;
-			while (pow > 0)
-			{
-				result = result * 2;
-				pow--;
-			}
-			sum = sum + result;
-		}
-		bin = bin / 10;
-		i++;
-	}
-	return (sum);
+	return (result);
 }

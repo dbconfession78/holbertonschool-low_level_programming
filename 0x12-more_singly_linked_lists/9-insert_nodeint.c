727461795c103cf7f7ddc61b3b836281f9de35fe
Stuart <108@holbertonschool.com>
2017-03-16 00:23:08 +0000

    style cleanup

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -20,7 +20,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	new_node->n = n;
 	if (idx == 0)
 	{
-		new_node-> next = *head;
+		new_node->next = *head;
 		*head = new_node;
 		return (new_node);
 	}
@@ -34,7 +34,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			temp_node = temp_node->next;
 			if (!temp_node)
 			{
-				free (new_node);
+				free(new_node);
 				return (NULL);
 			}
 		}

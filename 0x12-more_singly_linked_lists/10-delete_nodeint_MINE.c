9d66ee918736fe8bd859dfb9482f40cbeba7a755
Stuart <108@holbertonschool.com>
2017-03-16 05:46:22 +0000

    test

--- 0x12-more_singly_linked_lists/10-delete_nodeint_MINE.c
+++ 0x12-more_singly_linked_lists/10-delete_nodeint_MINE.c
@@ -9,26 +9,28 @@
 
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
+	listint_t *this;
+	listint_t *temp_node;
 	unsigned int i;
-	listint_t *current, *subsequent;
 
 	if (!head || !*head)
 		return (-1);
-	current = *head;
+	this = *head;
 	if (index == 0)
 	{
 		*head = (*head)->next;
-		free(current);
+		free(this);
 		return (1);
 	}
-	for (i = 0; i < (index - 1); i++)
+	for (i = 0; i < (index-1); i++)
 	{
-		current = current->next;
-		if (current == NULL)
+		this = this->next;
+		if (!this)
 			return (-1);
+
 	}
-	subsequent = current->next;
-	current->next = subsequent->next;
-	free(subsequent);
+	temp_node = this->next;
+	this->next = temp_node->next;
+	free(temp_node);
 	return (1);
 }

63931dd29c4823000c84a21fef5e35987569bf05
Stuart <108@holbertonschool.com>
2017-03-17 03:50:57 +0000

    3-set_bit.c: intial commit

---
+++ 0x13-bit_manipulation/3-set_bit.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ * set_bit - sets the value of a bit to 1 at a given index
+ * @n: number to set given index of
+ * @index: position to set bit at
+ * Return: 1 if succesfull; -1 if failed
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	return  ((*n |= 1 << index) ? 1 : -1);
+}

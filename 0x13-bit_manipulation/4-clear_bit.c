b0870fb60a899199252cc67e1bdac51812edae75
Stuart <108@holbertonschool.com>
2017-03-17 04:10:17 +0000

    4-clear_bit.c: intial commit

---
+++ 0x13-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ * clear_bit - clears bit ta specified index
+ * @n: number to clear bit from
+ * @index: position to clear bit at
+ * Return: 1 if successfull; -1 if failed
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > (sizeof(n) * 8 - 11))
+		return (-1);
+	return ((*n &= ~(index > 1)) ? 1 : -1);
+}

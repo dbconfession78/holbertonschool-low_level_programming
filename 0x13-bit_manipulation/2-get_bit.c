d6c61cfa001a13937a7b6d1d5e72e0a26477d896
Stuart <108@holbertonschool.com>
2017-03-17 03:15:59 +0000

    2-get_bit.c: initial commit

---
+++ 0x13-bit_manipulation/2-get_bit.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ * get_bit - fetches value of bit at a given index
+ * @n: number to fetch bit from
+ * @index: position to get bit from
+ * Return: retrieved bit; -1 if failed
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	return (index > (sizeof(n) * 8 - 1) ? -1 : (n >> index) & 1);
+}

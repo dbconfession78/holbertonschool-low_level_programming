8ee38fa7dbecc8fc1454d79efe4bca39be0155c9
Stuart <sgkur04@gmail.com>
2017-03-17 22:46:00 +0000

    initial commit 0-sum_them_all.c, variadic_functions.h, delete holberton.h

--- 0x0F-variadic_functions/0-sum_them_all.c
+++ 0x0F-variadic_functions/0-sum_them_all.c
@@ -4,17 +4,25 @@
 #include <limits.h>
 #include <stdarg.h>
 
+/**
+ * sum_them_all - takes any number of integeres and adds them
+ *
+ * @n: number of arguments
+ *
+ * Return: sum of arguments
+ */
 
-
-void function (void)
+int sum_them_all(const unsigned int n, ...)
 {
+	va_list list;
+	unsigned int i, sum = 0;
 
-}
+	if (n == 0)
+		return (0);
 
-int main(void)
-{
+	va_start(list, n);
+	for (i = 0; i < n; i++)
+		sum += va_arg(list, int);
 
-
-	return(0);
-
+	return (sum);
 }

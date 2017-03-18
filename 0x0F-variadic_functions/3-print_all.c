4e19696beea9779639aed6fb356bde85092a4083
Stuart <sgkur04@gmail.com>
2017-03-18 02:21:31 +0000

    3-print_all.c: initial commit, variadic_functions.h: include stdio.h and stdarg.h libraries

---
+++ 0x0F-variadic_functions/3-print_all.c
@@ -0,0 +1,51 @@
+#include "variadic_functions.h"
+
+/**
+ * print_all - prints anything
+ *
+ * @format: string where each char represents a data type
+ *
+ * Return: void
+ */
+
+void print_all(const char * const format, ...)
+{
+	va_list list;
+	int printed, i = 0;
+	char *str;
+
+	va_start(list, format);
+	while (format[i])
+	{
+		printed = 1;
+		switch (format[i])
+		{
+		case 's':
+			str = va_arg(list, char*);
+			if (!str)
+			{
+				printf("(nil)");
+				break;
+			}
+			printf("%s", str);
+			break;
+		case 'c':
+			printf("%c", va_arg(list, int));
+			break;
+		case 'f':
+			printf("%f", va_arg(list, double));
+			break;
+		case 'i':
+			printf("%d", va_arg(list, int));
+			break;
+		default:
+			printed = 0;
+			break;
+		}
+		if (format[i + 1] && printed == 1)
+			printf(", ");
+		i++;
+	}
+	va_end(list);
+	printf("\n");
+}

3ad8bc90d18cef93c383d116c6199674fb9544a5
Stuart <108@holbertonschool.com>
2017-04-03 23:13:44 +0000

    revert: pass but unsure why

--- 0x14-file_io/3-cp.c
+++ 0x14-file_io/3-cp.c
@@ -1,5 +1,5 @@
 #include "holberton.h"
-#define BUFSIZE 1024
+#define BUFFER_SIZE 1024
 
 /**
  * main - program entry point
@@ -9,41 +9,33 @@
  */
 int main(int argc, char *argv[])
 {
-	int file_from, file_to, readval, retval;
-	char buf[BUFSIZE];
+	int fd_from, fd_to, input, output;
+	char buffer[BUFFER_SIZE];
 
-	retval = 1;
+	output = 1;
 	if (argc != 3)
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"),
-			exit(97);
-	if (argv[1] == NULL)
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]),
-			exit(98);
-	if (argv[2] == NULL)
-		dprintf(STDERR_FILENO, "Erro\r: Can't write to file %s\n", argv[2]),
-			exit(99);
-	file_from = open(argv[1], O_RDONLY);
-	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC,
-				   S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
-	readval = read(file_from, buf, BUFSIZE);
-	if (readval == -1)
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]),
-			exit(98);
-	while (readval != 0)
+		dprintf(2, "Usage: cp file_from file_to\n"), exit(97);
+	if (!argv[1])
+		dprintf(2, "Error: Can't read from file %s\n", argv[1]), exit(98);
+	if (!argv[2])
+		dprintf(2, "Error: Can't write to %s\n", argv[2]), exit(99);
+	fd_from = open(argv[1], O_RDONLY);
+	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	input = read(fd_from, buffer, BUFFER_SIZE);
+	if (input == -1)
+		dprintf(2, "Error: Can't read from file %s\n", argv[1]), exit(98);
+	while (input != 0)
 	{
-		retval = write(file_to, buf, readval);
-		if (retval == -1 || retval != readval)
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
-		readval = read(file_from, buf, BUFSIZE);
-		if (readval == -1)
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]),
-				exit(98);
+		output = write(fd_to, buffer, input);
+		if (output == -1 || output != input)
+			dprintf(2, "Error: Can't write to %s\n", argv[2]), exit(99);
+		input = read(fd_from, buffer, BUFFER_SIZE);
+		if (input == -1)
+			dprintf(2, "Error: Can't read from file %s\n", argv[1]), exit(98);
 	}
-	retval = close(file_from);
-	if (retval == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from), exit(100);
-	retval = close(file_to);
-	if (retval == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to), exit(100);
+	if (close(fd_from) == -1)
+		dprintf(2, "Error: Can't close fd %d\n", fd_from), exit(100);
+	if (close(fd_to) == -1)
+		dprintf(2, "Error: Can't close fd %d\n", fd_to), exit(100);
 	return (0);
 }

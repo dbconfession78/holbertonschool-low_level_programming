72c71957c46c71a2fc5a86ec3658297b5c577287
Stuart <108@holbertonschool.com>
2017-04-03 20:55:35 +0000

    add close(fd) to write error handler

--- 0x14-file_io/2-append_text_to_file.c
+++ 0x14-file_io/2-append_text_to_file.c
@@ -26,7 +26,10 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 	if (write(fd, text_content, strlen(text_content)) == -1)
+	{
+		close(fd);
 		return (-1);
+	}
 	close(fd);
 	return (1);
 }

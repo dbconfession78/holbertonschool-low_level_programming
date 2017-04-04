3942a1dcd75804ab5e45c6c66d2dc8cae499c0f8
Stuart <108@holbertonschool.com>
2017-04-04 00:36:27 +0000

    refactor

--- 0x14-file_io/3-cp.c
+++ 0x14-file_io/3-cp.c
@@ -1,5 +1,5 @@
 #include "holberton.h"
-#define BUFSIZE 1024
+#define BUFFER_SIZE 1024
 
 /**
  * main - program entry point
@@ -9,41 +9,38 @@
  */
 int main(int argc, char *argv[])
 {
-	int file_from, file_to, readval, retval;
-	char buf[BUFSIZE];
+	int fd_from, fd_to, rd_val, wr_val;
+	char buffer[BUFFER_SIZE];
 
-	retval = 1;
+	wr_val = 1;
 	if (argc != 3)
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"),
-			exit(97);
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 	if (!argv[1])
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]),
 			exit(98);
 	if (!argv[2])
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]),
-			exit(99);
-	file_from = open(argv[1], O_RDONLY);
-	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC,
-				   0664);
-	readval = read(file_from, buf, BUFSIZE);
-	if (readval == -1)
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+	fd_from = open(argv[1], O_RDONLY);
+	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	rd_val = read(fd_from, buffer, BUFFER_SIZE);
+	if (rd_val == -1)
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]),
 			exit(98);
-	while (readval != 0)
+	while (rd_val != 0)
 	{
-		retval = write(file_to, buf, readval);
-		if (retval == -1 || retval != readval)
+		wr_val = write(fd_to, buffer, rd_val);
+		if (wr_val == -1 || wr_val != rd_val)
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
-		readval = read(file_from, buf, BUFSIZE);
-		if (readval == -1)
+		rd_val = read(fd_from, buffer, BUFFER_SIZE);
+		if (rd_val == -1)
 			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]),
 				exit(98);
 	}
-	retval = close(file_from);
-	if (retval == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from), exit(100);
-	retval = close(file_to);
-	if (retval == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to), exit(100);
+	wr_val = close(fd_from);
+	if (wr_val == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from), exit(100);
+	wr_val = close(fd_to);
+	if (wr_val == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to), exit(100);
 	return (0);
 }

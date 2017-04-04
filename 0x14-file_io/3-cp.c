d9bb007a728d506fc15be3a45600ea6801c75c3c
Stuart <108@holbertonschool.com>
2017-04-04 00:57:49 +0000

    move read/write/close error handlers to functions

--- 0x14-file_io/3-cp.c
+++ 0x14-file_io/3-cp.c
@@ -1,6 +1,23 @@
 #include "holberton.h"
 #define BUFFER_SIZE 1024
 
+void read_error(char *src_filename)
+{
+	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", src_file);
+	exit(98);
+}
+
+void write_error(char *target_filename)
+{
+	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+	exit(99);
+}
+
+void fd_close_error(int fd)
+{
+	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd), exit(100);
+}
+
 /**
  * main - program entry point
  * @argc: number count passed to program
@@ -16,31 +33,28 @@ int main(int argc, char *argv[])
 	if (argc != 3)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 	if (!argv[1])
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]),
-			exit(98);
+		read_error(argv[1]);
 	if (!argv[2])
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+		write_error(argv[2]);
 	fd_from = open(argv[1], O_RDONLY);
 	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	rd_val = read(fd_from, buffer, BUFFER_SIZE);
 	if (rd_val == -1)
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]),
-			exit(98);
+		read_error(argv[1]);
 	while (rd_val != 0)
 	{
 		wr_val = write(fd_to, buffer, rd_val);
 		if (wr_val == -1 || wr_val != rd_val)
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+			write_error(argv[2]);
 		rd_val = read(fd_from, buffer, BUFFER_SIZE);
 		if (rd_val == -1)
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]),
-				exit(98);
+			read_error(argv[1]);
 	}
 	wr_val = close(fd_from);
 	if (wr_val == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from), exit(100);
+		fd_close_error(fd_from);
 	wr_val = close(fd_to);
 	if (wr_val == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to), exit(100);
+		fd_close_error(fd_to);
 	return (0);
 }

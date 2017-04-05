8cd92af82c975e10ad6d10ae344043f90ef0a075
Stuart <sgkur04@gmail.com>
2017-04-05 02:59:07 +0000

    handle NULL content

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -27,12 +27,16 @@ int create_file(const char *filename, char *text_content)
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fd == -1)
 		return (-1);
-	if (text_content)
+	if (!text_content)
 	{
-		bytes_written = write(fd, text_content, content_len);
-		if (bytes_written == -1 || bytes_written != content_len)
+		if (close(fd) == -1)
 			return (-1);
+		else
+			return (1);
 	}
+	bytes_written = write(fd, text_content, content_len);
+	if (bytes_written == -1 || bytes_written != content_len)
+		return (-1);
 	if (close(fd) == -1)
 		return (-1);
 	return (1);

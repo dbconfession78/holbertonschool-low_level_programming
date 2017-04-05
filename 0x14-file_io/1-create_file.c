7f54a13377aa5d6eb78935a78c1ebed08dd1864d
Stuart <sgkur04@gmail.com>
2017-04-05 03:05:30 +0000

    passed: playing around with file

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -24,16 +24,13 @@ int create_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
-/*	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR); */
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
 	if (!text_content)
 	{
-		if (close(fd) == -1)
-			return (-1);
-		else
-			return (1);
+		close(fd);
+		return (1);
 	}
 	content_len = strlen(text_content);
 	bytes_written = write(fd, text_content, content_len);

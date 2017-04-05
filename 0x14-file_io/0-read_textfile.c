1c56420a474cde8397fb4fbbd22f87f76e1c7604
Stuart <sgkur04@gmail.com>
2017-04-05 01:06:28 +0000

    passed: cleaning up

--- 0x14-file_io/0-read_textfile.c
+++ 0x14-file_io/0-read_textfile.c
@@ -45,14 +45,11 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 
 	if (read(fd, buffer, letters) == -1)
-	{
-/*		free(buffer); */
 		return (0);
-	}
 	buffer_len = write(STDOUT_FILENO, buffer, _strlen(buffer));
 	if (buffer_len == -1)
 	{
-		free(buffer);
+/*		free(buffer); */
 		return (0);
 	}
 

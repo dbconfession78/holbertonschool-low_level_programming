42eb8a3ec7c056ed564ad45da6c87434e0a43e85
Stuart <sgkur04@gmail.com>
2017-04-05 02:49:05 +0000

    add 'content len' to 'written len' difference handler

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -20,6 +20,7 @@ int create_file(const char *filename, char *text_content)
 {
 	int fd;
 	int content_len = strlen(text_content);
+	int bytes_written = 0;
 
 	if (!filename)
 		return (-1);
@@ -27,7 +28,9 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 	if (text_content)
-		if (write(fd, text_content, content_len) == -1)
+	{
+		bytes_written = write(fd, texxt_content, content_len);
+		if (bytes_written == -1 || bytes_written != content_len)
 			return (-1);
 	if (close(fd) == -1)
 		return (-1);

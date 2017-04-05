38193fa9cd7f62fbe5b947c0bca3af7401575782
Stuart <sgkur04@gmail.com>
2017-04-05 01:05:21 +0000

    passed: cleaning up

--- 0x14-file_io/0-read_textfile.c
+++ 0x14-file_io/0-read_textfile.c
@@ -40,11 +40,10 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	if (!filename || !buffer)
 		return (0);
-
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 	{
-		free(buffer);
+		/* free(buffer); */
 		return (0);
 	}
 

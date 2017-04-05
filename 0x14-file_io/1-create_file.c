a7bcdd17a2a3780ef9d57c6ac3174ae883553c89
Stuart <sgkur04@gmail.com>
2017-04-05 02:16:07 +0000

    1-create_file.c: initial commit

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -2,33 +2,36 @@
 
 /**
  * create_file - creates a new file
+ *
  * @filename: name of file to create
  * @text_content: text written to file
+ *
  * Return: 1 if succesfully created file;
  * -1 if can't create or write file
  * -1 if filename is NULL
  */
 
-/* file must have rw-------- permissions */
+/* file must have rw------- permissions */
 /* if file exists, truncate it */
 /* if filename is NULL, return -1 */
 /* if can't create or write file, return -1 */
 /* if text content is NULL, create an empty file */
 int create_file(const char *filename, char *text_content)
 {
+	int fd;
+	int content_len = strlen(text_content);
 
-}
+	if (!filename)
+		return (-1);
 
-int main(int ac, char **av)
-{
-    int res;
+	if (access(filename, F_OK) == -1)
+		fd = open(filename, O_CREAT | O_WRONLY, 0600);
+	else
+		fd = open(filename, O_WRONLY);
+	if (text_content)
+		if (write(fd, text_content, content_len) == -1)
+			return (-1);
 
-    if (ac != 3)
-    {
-        dprintf(2, "Usage: %s filename text\n", av[0]);
-        exit(1);
-    }
-    res = create_file(av[1], av[2]);
-    printf("-> %i)\n", res);
-    return (0);
+	close(fd);
+	return (1);
 }

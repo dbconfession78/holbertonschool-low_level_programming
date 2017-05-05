924b0ff6c37279aefddcbcc098d1877825c247a0
Stuart Kuredjian <108@holbertonschool.com>
2017-05-05 21:02:42 +0000

    style fixes

--- 0x16-doubly_linked_lists/0-print_dlistint.c
+++ 0x16-doubly_linked_lists/0-print_dlistint.c
@@ -11,11 +11,12 @@
 size_t print_dlistint(const dlistint_t *h)
 {
 	size_t node_count = 0;
+
 	while (h)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
 		node_count++;
 	}
-	return node_count;
+	return (node_count);
 }

a84673886e6755e11a94d4f5e8c4468a752a441e
Stuart Kuredjian <108@holbertonschool.com>
2017-05-05 21:22:59 +0000

    2-add_dnodeint.c: initial commit

--- 0x16-doubly_linked_lists/2-add_dnodeint.c
+++ 0x16-doubly_linked_lists/2-add_dnodeint.c
@@ -8,5 +8,26 @@
 
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
+	dlistint_t *new;
+	dlistint_t *temp;
 
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->n = n;
+	new->next = NULL;
+	new->prev = NULL;
+
+	if (*head == NULL)
+	{
+		*head = new;
+		return (*head);
+	}
+
+	temp = *head;
+	new->next = temp;
+	temp->prev = new;
+	*head = new;
+	return (*head);
 }

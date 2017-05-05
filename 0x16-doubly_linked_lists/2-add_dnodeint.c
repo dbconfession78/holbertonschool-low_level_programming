c191dd72dcb62f6af5cbc88679863125dde061f3
Stuart Kuredjian <108@holbertonschool.com>
2017-05-05 22:53:30 +0000

    refactor

--- 0x16-doubly_linked_lists/2-add_dnodeint.c
+++ 0x16-doubly_linked_lists/2-add_dnodeint.c
@@ -5,29 +5,15 @@
  * @n: value of node's int element
  * Return: address of new element or NULL if failed
  */
-
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	dlistint_t *new;
-	dlistint_t *temp;
+	dlistint_t *new = malloc(sizeof(dlistint_t));
 
-	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 		return (NULL);
-
 	new->n = n;
-	new->next = NULL;
 	new->prev = NULL;
-
-	if (*head == NULL)
-	{
-		*head = new;
-		return (*head);
-	}
-
-	temp = *head;
-	new->next = temp;
-	temp->prev = new;
+	new->next = *head;
 	*head = new;
 	return (*head);
 }

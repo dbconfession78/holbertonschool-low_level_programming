2211b50cadab2437fe59542825fbb02609d18429
Stuart Kuredjian <108@holbertonschool.com>
2017-05-08 19:45:39 +0000

    add error handle for condition: NULL head but index isn't 0

--- 0x16-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x16-doubly_linked_lists/7-insert_dnodeint.c
@@ -13,6 +13,9 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *new = malloc(sizeof(dlistint_t));
 	unsigned int i = 0;
 
+	if (h == NULL && idx != 0)
+		return (NULL);
+
 	if (new == NULL)
 		return (NULL);
 	new->n = n;

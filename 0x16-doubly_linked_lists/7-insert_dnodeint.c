f2a9346f783a95a3128c8204697c1daa23ef25f3
Stuart Kuredjian <108@holbertonschool.com>
2017-05-08 20:56:55 +0000

    test

--- 0x16-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x16-doubly_linked_lists/7-insert_dnodeint.c
@@ -16,8 +16,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (new == NULL)
 		return (NULL);
 	new->n = n;
-	new->prev = NULL;
-	new->next = NULL;
 	if (idx == 0)
 	{
 		new->prev = NULL;

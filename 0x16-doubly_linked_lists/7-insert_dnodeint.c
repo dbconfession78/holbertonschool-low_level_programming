10a750258c776cf8d39f6a35eefe1a77d1624230
Stuart Kuredjian <108@holbertonschool.com>
2017-05-08 19:38:54 +0000

    fix value of node after new node is inserted

--- 0x16-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x16-doubly_linked_lists/7-insert_dnodeint.c
@@ -42,6 +42,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		new->prev = temp;
 		new->next = temp->next;
 		temp->next = new;
+		new->next->prev = new;
 	}
 	return (NULL);
 }

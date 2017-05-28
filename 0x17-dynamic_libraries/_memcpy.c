cb85c12cdf335cd230d44463d0b84601c2f04a46
Stuart Kuredjian <108@holbertonschool.com>
2017-05-28 23:52:32 +0000

    remove test.c

---
+++ 0x17-dynamic_libraries/_memcpy.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * _memcpy - copies memory area
+ *
+ * @dest: location to copy to
+ * @src: location to copy from
+ * @n: number of bytes to copy
+ *
+ * Return: memory filled array
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	 unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	return (dest);
+}
---
+++ 0x17-dynamic_libraries/lib/_strspn.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * _strspn - get length of a substring
+ *
+ * @s: string to search
+ * @accept: character set to search for
+ *
+ * Return: length of substring
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i, j;
+
+	for (i = 0; s[i]; i++)
+	{
+		for (j = 0; accept[j]; j++)
+		{
+			if (s[i] == accept[j])
+				break;
+		}
+		if (!accept[j])
+			break;
+	}
+	return (i);
+}

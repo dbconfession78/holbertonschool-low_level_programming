d203895f6d60ed5515ff313580e29bc083ff4b5d
Stuart Kuredjian <108@holbertonschool.com>
2017-05-31 21:38:23 +0000

    remove NULL loop

--- 0x19-hash_tables/0-hash_table_create.c
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -11,7 +11,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *ht;
 	unsigned int i;
 
-	if (size <= 0)
+	if (size == 0)
 		return (NULL);
 
 	ht = malloc(sizeof(hash_table_t));
@@ -21,10 +21,8 @@ hash_table_t *hash_table_create(unsigned long int size)
 	ht->size = size;
 	ht->array = malloc(sizeof(hash_node_t *) * size);
 	if (ht->array == NULL)
-		free(ht);
+		return (NULL);
 
-	for (i = 0; i < size; i++)
-		ht->array[i] = NULL;
 
 	return (ht);
 

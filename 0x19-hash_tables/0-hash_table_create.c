7136bf19af0e4d78b68020b3b2328a14aa6c74f1
Stuart Kuredjian <108@holbertonschool.com>
2017-05-31 21:45:21 +0000

    remove documentation to see if that helps tests pass

--- 0x19-hash_tables/0-hash_table_create.c
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -1,10 +1,6 @@
 #include "hash_tables.h"
 
-/**
- * hash_table_create - creates a hash table
- * @size: size of the hashtable (2D array)
- * Return: pointer to the newly created hash table
- */
+
 
 hash_table_t *hash_table_create(unsigned long int size)
 {

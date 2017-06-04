1c8d224ece2c839d8a6ed7612ff7ed036bb25f8c
Stuart <sgkur04@gmail.com>
2017-06-04 22:06:58 +0000

    add {} to while loop at L25

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -8,16 +8,13 @@
  * Return: 1 on success; 0 on fail
  */
 
-
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *node;
-	hash_node_t *temp;
+	hash_node_t *node, *temp;
 	unsigned long int index;
 
 	if (!ht || !key || value || !ht->array || strlen(key) == 0)
 		return (0);
-
 	/* hash the index */
 	index = key_index((const unsigned char *)key, ht->size);
 	/* if that index exists, set a pointer to it */
@@ -26,6 +23,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		temp = ht->array[index];
 		/* then move through the linked list */
 		while (temp != NULL)
+		{
 			/* if the key is found , replace it's value */
 			if (strcmp(key, temp->key) == 0)
 			{
@@ -33,7 +31,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 				temp->value = strdup(value);
 				return (1);
 			}
-		temp = temp->next;
+			temp = temp->next;
+		}
 	}
 	node = malloc(sizeof(hash_node_t));
 	if (node == NULL)

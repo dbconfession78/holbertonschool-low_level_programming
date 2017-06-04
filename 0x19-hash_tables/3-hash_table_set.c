06ee15e800e6e1344c2673a3752037110bd40f5c
Stuart <sgkur04@gmail.com>
2017-06-04 22:15:14 +0000

    use ! for strcmp instead of == 0 at L28

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -25,7 +25,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		while (temp != NULL)
 		{
 			/* if the key is found , replace it's value */
-			if (strcmp(key, temp->key) == 0)
+			if (!strcmp(key, temp->key))
 			{
 				free(temp->value);
 				temp->value = strdup(value);

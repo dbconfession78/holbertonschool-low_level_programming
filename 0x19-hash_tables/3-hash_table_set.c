2b83580dd52bd05787d2eb8ca51b22ee919a89a1
Stuart <sgkur04@gmail.com>
2017-06-04 22:09:25 +0000

    add ! to value at L16

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -13,7 +13,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *node, *temp;
 	unsigned long int index;
 
-	if (!ht || !key || value || !ht->array || strlen(key) == 0)
+	if (!ht || !key || !value || !ht->array || strlen(key) == 0)
 		return (0);
 	/* hash the index */
 	index = key_index((const unsigned char *)key, ht->size);

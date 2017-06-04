59568a386c5d37201a42ff96dbe06b7821596bc4
Stuart <sgkur04@gmail.com>
2017-06-04 22:18:43 +0000

    change new_node->next from =temp to =node

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -46,7 +46,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		free(node);
 		return (0);
 	}
-	node->next = temp;
+	node->next = node;
 	ht->array[index] = node;
 	return (1);
 }

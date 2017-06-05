d36bd57d205e96ef87b8541d05641f61b38e17a1
Stuart <sgkur04@gmail.com>
2017-06-05 01:04:53 +0000

    new version: initial commit

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -10,42 +10,47 @@
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *node, *temp, *head;
-	unsigned long int index;
+	hash_node_t *node;
+	hash_node_t *temp;
+	unsigned long int hash;
 
-	if (!ht || !key || !value || !ht->array || strlen(key) == 0)
+	if (!ht || !key || !value)
 		return (0);
-	/* hash the index */
-	index = key_index((unsigned char *)key, ht->size);
-	head = temp = ht->array[index];
-	if (head)
+
+	/* create hashed index */
+	hash = key_index((unsigned char *) key, ht->size);
+	temp = ht->array[hash];
+
+	while(temp != NULL)
 	{
-		/* then move through the linked list */
-		while (temp != NULL)
+		if (strcmp(key, temp->key) == 0)
 		{
-			/* if the key is found , replace it's value */
-			if (!strcmp(temp->key, key))
-			{
-				free(temp->value);
-				temp->value = strdup(value);
-				return (1);
-			}
-			temp = temp->next;
+			free(temp->value);
+			temp->value = strdup(value);
+			return (1);
 		}
+		temp = temp->next;
 	}
+
 	node = malloc(sizeof(hash_node_t));
-	if (!node)
+	if (node == NULL)
 		return (0);
 	node->key = strdup(key);
-	node->value = strdup(value);
-	if (!node->key || !node->value)
+	if (node->key == NULL)
 	{
-		if (node->key)
-			free(node->key);
 		free(node);
 		return (0);
 	}
-	node->next = head;
-	ht->array[index] = node;
+	node->value = strdup(value);
+	if (node->value == NULL)
+	{
+		free(node->key);
+		free(node);
+		return (0);
+	}
+	node->next = ht->array[hash];
+	ht->array[hash] = node;
+
 	return (1);
+
 }

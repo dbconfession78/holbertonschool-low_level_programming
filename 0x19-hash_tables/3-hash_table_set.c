bec89ef8f264beaff1c52b1a242530a3cd19394d
Stuart <sgkur04@gmail.com>
2017-06-05 00:14:19 +0000

    test

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -47,7 +47,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (!ht || !key || !value || !ht->array || strlen(key) == 0)
 		return (0);
 	index = key_index((unsigned char *)key, ht->size);
-	head = temp = ht->array[idx];
+	head = temp = ht->array[index];
 	if (head)
 	{
 		while (temp)

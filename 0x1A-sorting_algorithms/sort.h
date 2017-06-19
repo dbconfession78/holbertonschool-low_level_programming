35fcc07947274f3892ffc7f611ae2361dc308471
Stuart Kuredjian <108@holbertonschool.com>
2017-06-19 05:18:34 +0000

    sort.h: remove prototype for count_list

--- 0x1A-sorting_algorithms/sort.h
+++ 0x1A-sorting_algorithms/sort.h
@@ -26,5 +26,4 @@ void swap(int *a, int *b);
 int partition(int *array, int lo, int hi, size_t size);
 void do_quick_sort(int *array, int lo, int hi, size_t size);
 int array_cmp(int array_1[], int array_2[], size_t size);
-unsigned int count_list(listint_t **list);
 #endif

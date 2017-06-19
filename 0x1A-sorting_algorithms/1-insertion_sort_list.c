af234e71dc6d4fba927e8d81948ceb0c56e6bd0c
Stuart Kuredjian <108@holbertonschool.com>
2017-06-19 23:51:52 +0000

    add temp->prev =  NULL when temp is put at head, and make last node's next == NULL

--- 0x1A-sorting_algorithms/1-insertion_sort_list.c
+++ 0x1A-sorting_algorithms/1-insertion_sort_list.c
@@ -12,40 +12,40 @@ void insertion_sort_list(listint_t **list)
 
 	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
-
 	temp = (*list)->next;
 	nxt = temp->next; prv = temp->prev;
 	while (temp)
 	{
 		if (temp->n < prv->n)
-		{
 			while (prv && temp->n < prv->n)
 			{
+				if (temp->next)
+					temp->next->prev = temp->prev;
 				/* next */
 				if (nxt)
 					nxt->prev = prv;
-
+				else
+					temp->next = NULL;
 				/* temp */
 				temp->next = prv;
 				temp->prev = prv->prev;
-
 				/* prev */
 				if (prv->prev)
 					prv->prev->next = temp;
-				prv->prev = temp;
-				prv->next = nxt;
-
+				else
+				{
+					temp->prev = NULL; temp->next = *list;
+					(*list)->prev = temp; *list = temp;
+				}
+				prv->prev = temp; prv->next = nxt;
 				nxt = prv;
 				prv = temp->prev;
 				if ((*list)->prev)
 					*list = (*list)->prev;
 				print_list(*list);
 			}
-		}
 		temp = nxt;
 		if (temp != NULL)
-		{
 			nxt = temp->next; prv = temp->prev;
-		}
 	}
 }

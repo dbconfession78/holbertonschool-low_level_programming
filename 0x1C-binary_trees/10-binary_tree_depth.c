a79ab303aeac9e549364b727133f64a86aca6711
Stuart <sgkur04@gmail.com>
2017-07-25 20:09:11 +0000

    10-binary_tree_depth.c :replaced recursion with while loop

--- 0x1C-binary_trees/10-binary_tree_depth.c
+++ 0x1C-binary_trees/10-binary_tree_depth.c
@@ -1,28 +1,22 @@
 #include "binary_trees.h"
 
 /**
- * get_binary_tree_depth - measures the depth of a binary tree
- * @tree: pointer to root node of tree to measure
- * @depth: current depth
- * Return: depth of tree or 0 if node is NULL
- */
-size_t get_binary_tree_depth(const binary_tree_t *tree, size_t depth)
-{
-	if (tree == NULL || tree->parent == NULL)
-		return (depth);
-
-	return (get_binary_tree_depth(tree->parent, depth + 1));
-}
-
-/**
  * binary_tree_depth - measures the depth of a binary tree
  * @node: pointer to root node of tree to measure
  * Return: depth of tree or 0 if node is NULL
  */
 size_t binary_tree_depth(const binary_tree_t *node)
 {
+	size_t depth;
+
 	if (node == NULL)
 		return (0);
 
-	return (get_binary_tree_depth(node, (size_t) 0));
+	depth = 0;
+	while (node->parent != NULL)
+	{
+		node = node->parent;
+		depth++;
+	}
+	return (depth);
 }

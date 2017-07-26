6bb4621548a96119c1defbfb4d640c50ea168914
Stuart Kuredjian <108@holbertonschool.com>
2017-07-26 22:51:27 +0000

    12-binary_tree_leaves.c :add NULL tree handler

--- 0x1C-binary_trees/12-binary_tree_leaves.c
+++ 0x1C-binary_trees/12-binary_tree_leaves.c
@@ -1,41 +1,10 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_insert_left - inserts a node as a left-child of another node
- * @parent: the parent node
- * @value: the value at the current node
- * Return: pointer to the created node or NULL on failure
- */
-
-binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
-{
-	binary_tree_t *new_node;
-
-/*	if (parent == NULL)
-		return (NULL);
-*/
-
-	new_node = binary_tree_node(parent, value);
-	if (new_node == NULL)
-		return (NULL);
-
-	if (parent->left == NULL)
-		parent->left = new_node;
-	else
-	{
-		new_node->left = parent->left;
-		parent->left->parent = new_node;
-		parent->left = new_node;
-	}
-	return (new_node);
-}
-
-/**
  * binary_tree_is_leaf - checks if a node is a leaf
  * @node: pointer to the node to check
  * Return: 1 if leaf. 0 if not leaf or node is NULL
  */
-
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
 	if (node == NULL || (node->left != NULL || node->right != NULL))
@@ -53,6 +22,9 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 {
 	size_t leaves = 0;
 	size_t is_leaf = 0;
+
+	if (tree == NUll)
+		return (0);
 
 	if (tree)
 	{
